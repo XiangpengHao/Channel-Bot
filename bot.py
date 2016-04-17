3590d7a208822e7fe5c13bcb300bd550a1be7707
Hao Xiangpeng <3150102255@zju.edu.cn>
2016-04-17 18:43:04 +0800

    Update API, add free chat.

--- bot.py
+++ bot.py
@@ -1,33 +1,61 @@
-from telegram import Updater
+import turingChat
+
+from telegram.ext import Updater
 import logging
 
 logging.basicConfig(
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        level=logging.INFO)
-logger =logging.getLogger(__name__)
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-def start(bot,update):
-    bot.sendMessage(update.message.chat_id,text='Hi')
 
-def best(bot,update):
-    bot.sendMessage(update.message.chat_id,text='Python is the best language ever.')
+def start(bot, update):
+    bot.sendMessage(update.message.chat_id, text='Hi')
 
-def help(bot,update):
-    bot.sendMessage(update.message.chat_id,text='This bot is created by Patrick Hao')
 
-def echo(bot,update):
-    bot.sendMessage(update.message.chat_id,text=update.message.text[5:])
+def best(bot, update):
+    bot.sendMessage(update.message.chat_id, text='Python is the best language ever.')
+
+
+def help(bot, update):
+    bot.sendMessage(update.message.chat_id, text='This bot is created by Patrick Hao')
+
+
+def echo(bot, update):
+    bot.sendMessage(update.message.chat_id, text=update.message.text[5:])
+
+
+def chat(bot, update):
+    chat_message = update.message.text[5:]
+    send_message = turingChat.turning_chat(chat_message, turing_key)
+    bot.sendMessage(update.message.chat_id, text=send_message)
+
+
+def get_key():
+    try:
+        with open('key.txt') as key_file:
+            tele_key = key_file.readline()
+            turing_key = key_file.readline()
+        return tele_key, turing_key
+    except Exception as e:
+        print(e)
+        exit(0)
+
 
 def main():
-    updater=Updater("149794753:AAEv4zJDt5IPY-A8sBEwo5I2p_9MT_TldZw")
-    dp=updater.dispatcher
-    dp.addTelegramCommandHandler("start",start)
-    dp.addTelegramCommandHandler("help",help)
-    dp.addTelegramCommandHandler("best",best)
-    dp.addTelegramCommandHandler("echo",echo)
+    my_updater = Updater(tele_key)
+    dp = my_updater.dispatcher
+    dp.addTelegramCommandHandler("start", start)
+    dp.addTelegramCommandHandler("help", help)
+    dp.addTelegramCommandHandler("best", best)
+    dp.addTelegramCommandHandler("echo", echo)
+    dp.addTelegramCommandHandler("chat", chat)
 
-    updater.start_polling()
-    updater.idle()
+    my_updater.start_polling()
+    my_updater.idle()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
+    tele_key, turing_key = get_key()
+    tele_key = tele_key[:-1]
     main()
---
+++ turingChat.py
@@ -0,0 +1,29 @@
+import urllib.parse, urllib.request, json
+
+
+def turning_chat(message, key):
+    post_value = {'key': key,
+                  'info': message,
+                  'userid': 'haoxiangpeng'}
+    encoded_info = urllib.parse.urlencode(post_value)
+    url = 'http://www.tuling123.com/openapi/api?%s' % encoded_info
+    print(url)
+    response_content = urllib.request.urlopen(url).read().decode('utf-8')
+    print(response_content)
+    json_parsed_content = json.loads(response_content)
+    if json_parsed_content['code'] == 200000:
+        return_message = ('%s\n%s' % (json_parsed_content['text'], json_parsed_content['url']))
+    elif json_parsed_content['code'] == 100000:
+        return_message = json_parsed_content['text']
+    elif json_parsed_content['code'] == 302000:
+        return_message = ('%s:\n%s\n%s\n\n%s\n%s\n\n%s\n%s' % (json_parsed_content['text'],
+                                                               json_parsed_content['list'][0]['article'],
+                                                               json_parsed_content['list'][0]['detailurl'],
+                                                               json_parsed_content['list'][1]['article'],
+                                                               json_parsed_content['list'][1]['detailurl'],
+                                                               json_parsed_content['list'][2]['article'],
+                                                               json_parsed_content['list'][2]['detailurl']
+                                                               ))
+    else:
+        return_message = 'Encountered unknown error'
+    return return_message
