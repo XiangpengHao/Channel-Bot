59a1b6b76bbcb632c3296cfa28b876701ebf6c67
haoxiangpeng <3150102255@zju.edu.cn>
2017-12-18 11:13:15 -0800

    :wrench: modify ignore

---
+++ config.py
@@ -0,0 +1,13 @@
+tokens = {
+  "bot": '414074392:AAFvB3F5bCXo6J04sY-yt3yrD762rFAWwCk',
+  'newsapi': '2e1c752fee3a4ad7baae59fa96fd6ba8',
+  'weather': '44ef9eb2015c4e40b9f174719170309',
+  'allowed_id': [-1001128650063, 132580810]
+}
+
+MYSQL_CONFIG = {
+  'host_name': 'maze.haoxp.xyz',
+  'server_port': 3306,
+  'user': 'root',
+  'password': '6wnnNfg9py0Y'
+}
