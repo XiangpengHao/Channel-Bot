1f570110a2bc66678f24b9a9317373f64f7029ac
haoxiangpeng <3150102255@zju.edu.cn>
2017-12-19 12:07:24 -0800

    :zap: change ignore

---
+++ sample_rv.py
@@ -0,0 +1,137 @@
+{
+  "theverge": [{
+    "author": "Dan Seifert",
+    "title": "Amazon Echo Spot review: an almost-perfect smart alarm clock",
+    "description": "‘Alexa, are you watching me?’",
+    "url": "https://www.theverge.com/2017/12/18/16787600/amazon-echo-spot-alexa-clock-review",
+    "urlToImage": "https://cdn.vox-cdn.com/thumbor/tE8asTpYnI75DsqdW9VA86C1ia8=/0x292:2040x1360/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/9876657/akrales_171214_2193_0008.jpg",
+    "publishedAt": "2017-12-18T17:00:02Z"
+  }, {
+    "author": "Adi Robertson",
+    "title": "Apple let a fake $5 Cuphead game into the App Store",
+    "description": "A version of run-and-gun game Cuphead unexpectedly showed up on iOS — but it’s a scam. TouchArcade reported that an iOS version had been published today, linking to what seems like a legitimate App...",
+    "url": "https://www.theverge.com/2017/12/18/16789870/apple-ios-fake-cuphead-game-app-store-studiomdhr",
+    "urlToImage": "https://cdn.vox-cdn.com/thumbor/gpn2OGsHuQ9UyNP6jpzte65ovJ8=/0x146:2040x1214/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/9889241/DSCF2579.jpg",
+    "publishedAt": "2017-12-18T14:11:31Z"
+  }, {
+    "author": "Loren Grush",
+    "title": "The interstellar asteroid visiting our Solar System may be shielding an icy core",
+    "description": "`Oumuamua, you’re as cold as ice",
+    "url": "https://www.theverge.com/2017/12/18/16788354/oumuamua-interstellar-asteroid-outer-layer-ice-interior-comet",
+    "urlToImage": "https://cdn.vox-cdn.com/thumbor/AYV7mOrcYdTuOaTiC_h0p0zhziw=/0x65:1280x735/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/9890061/interstellar_asteroid.jpg",
+    "publishedAt": "2017-12-18T16:32:52Z"
+  }, {
+    "author": "Verge Staff",
+    "title": "The Verge Holiday Gift Guide 2017",
+    "description": "For as long as gift-giving has been a tradition, there’s also been the inability to come up with good gift ideas. Whether it’s for your mom, dad, sister, brother, partner, friend, colleague, they, them, him, her, cat, dog, secret Santa, or for a White Elephant party, it can often be tricky to select the perfect present for your recipient.",
+    "url": "https://www.theverge.com/2017/11/6/16581430/holiday-gift-ideas-2017-christmas-hannukah-guide",
+    "urlToImage": "https://cdn.vox-cdn.com/thumbor/tlJx-wNGUQmSA2ToJTqq1LnnhkU=/0x0:2040x1068/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/9577591/akrales_171019_2067_lede_0017_final.jpg",
+    "publishedAt": "2017-11-06T14:00:02Z"
+  }, {
+    "author": "Angela Chen",
+    "title": "At age six, children develop a sense of justice",
+    "description": "Before that, watch out",
+    "url": "https://www.theverge.com/2017/12/18/16789966/justice-fairness-psychology-children",
+    "urlToImage": "https://cdn.vox-cdn.com/thumbor/QgUf1_pzan8r4XqGJZt8j0QQV9Y=/0x248:3456x2057/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/9890285/381891241_2e3db14f40_o.jpg",
+    "publishedAt": "2017-12-18T16:26:10Z"
+  }, {
+    "author": "Jacob Kastrenakes",
+    "title": "Does phone radiation give you cancer?",
+    "description": "Probably not, but scientists are still looking into it",
+    "url": "https://www.theverge.com/circuitbreaker/2017/12/18/16751954/phone-radiation-cancer-link-studies",
+    "urlToImage": "https://cdn.vox-cdn.com/thumbor/T4jDbUmMgHE_m1zkPYrrxifHVTY=/0x29:745x419/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/9890581/t.png",
+    "publishedAt": "2017-12-18T17:05:13Z"
+  }, {
+    "author": "Thuy Ong",
+    "title": "Twitter starts enforcing new policies on violence, abuse, and hateful conduct",
+    "description": "Policies now extend to hateful imagery and to conduct both on platform and off",
+    "url": "https://www.theverge.com/2017/12/18/16789606/twitter-new-safety-policies-hate-groups",
+    "urlToImage": "https://cdn.vox-cdn.com/thumbor/lpl2YiZCk7t89kUBUZJHCnpnvEE=/0x210:3000x1781/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/9888917/187243543.jpg.jpg",
+    "publishedAt": "2017-12-18T14:00:08Z"
+  }, {
+    "author": "Shannon Liao",
+    "title": "WhatsApp ordered to stop sharing user data with Facebook",
+    "description": "France’s ultra-strict privacy watchdog CNIL has ordered WhatsApp to stop sharing user data with parent company Facebook. The app has a month to comply with the order, according to a public notice...",
+    "url": "https://www.theverge.com/2017/12/18/16792448/whatsapp-facebook-data-sharing-no-user-consent",
+    "urlToImage": "https://cdn.vox-cdn.com/thumbor/1V7yh0BGKyYjEPMCSwJSk6H3NGE=/0x13:3000x1584/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/9892767/586113572.jpg.jpg",
+    "publishedAt": "2017-12-18T22:06:20Z"
+  }, {
+    "author": "Chaim Gartenberg",
+    "title": "Adidas will quit making wearable fitness devices",
+    "description": "The company has cut the division developing its fitness watches",
+    "url": "https://www.theverge.com/circuitbreaker/2017/12/18/16792684/adidas-wearable-fitness-devices-gps-watch-business-shut-down",
+    "urlToImage": "https://cdn.vox-cdn.com/thumbor/5mwYtvHAqIgtoUn9Y9rGNH6d7vs=/0x383:1100x959/fit-in/1200x630/cdn.vox-cdn.com/assets/3406055/304086.jpg",
+    "publishedAt": "2017-12-18T21:58:09Z"
+  }],
+  "theeconomist": [{
+    "author": "The Economist",
+    "title": "What Sebastián Piñera’s victory in Chile’s presidential election means",
+    "description": "A resounding win for the conservative candidate reaffirms Chileans’ centrist leanings",
+    "url": "http://www.economist.com/news/americas/21732794-resounding-win-conservative-candidate-reaffirms-chileans-centrist-leanings-what",
+    "urlToImage": "https://cdn.static-economist.com/sites/default/files/images/2017/12/articles/main/20171223_amp001.jpg",
+    "publishedAt": "2017-12-18T00:00:00Z"
+  }, {
+    "author": "The Economist",
+    "title": "South Africa’s ruling party rejects the Zuma family",
+    "description": "The ditching of a dismal dynasty restores hope to the rainbow nation",
+    "url": "http://www.economist.com/news/leaders/21732799-ditching-dismal-dynasty-restores-hope-rainbow-nation-south-africas-ruling-party",
+    "urlToImage": "https://cdn.static-economist.com/sites/default/files/images/2017/12/articles/main/20171223_map501.jpg",
+    "publishedAt": "2017-12-18T00:00:00Z"
+  }, {
+    "author": "The Economist",
+    "title": "To understand East Asian nationalism, climb a mountain",
+    "description": "One country’s peak is another’s poison",
+    "url": "http://www.economist.com/news/asia/21732530-one-countrys-peak-anothers-poison-understand-east-asian-nationalism-climb-mountain",
+    "urlToImage": "https://cdn.static-economist.com/sites/default/files/images/print-edition/20171216_ASD000_0.jpg",
+    "publishedAt": "2017-12-14T15:49:51Z"
+  }, {
+    "author": "The Economist",
+    "title": "Will America’s economy overheat in 2018?",
+    "description": "The labour market is the healthiest it has been for at least a decade. But inflation remains low",
+    "url": "http://www.economist.com/news/finance-and-economics/21732536-labour-market-healthiest-it-has-been-least-decade",
+    "urlToImage": "https://cdn.static-economist.com/sites/default/files/images/print-edition/20171216_FND001_0.jpg",
+    "publishedAt": "2017-12-14T15:49:51Z"
+  }, {
+    "author": "The Economist",
+    "title": "Why is America more tolerant of inequality than many rich countries?",
+    "description": "Ignorance about the scale of the problem is part of the answer",
+    "url": "http://www.economist.com/blogs/democracyinamerica/2017/12/capital-question",
+    "urlToImage": "//cdn.static-economist.com/assets/the-economist-logo.png",
+    "publishedAt": "2017-12-18T14:06:20Z"
+  }, {
+    "author": "The Economist",
+    "title": "What 2018 has in store for the markets",
+    "description": "Investors are very optimistic. But two analysts think they may be ignoring a big risk",
+    "url": "http://www.economist.com/blogs/buttonwood/2017/12/china-syndrome",
+    "urlToImage": "https://cdn.static-economist.com/sites/default/files/20171216_BLP526_0.jpg",
+    "publishedAt": "2017-12-18T10:59:06Z"
+  }, {
+    "author": "The Economist",
+    "title": "The Italian church is dissolving its links to the mob",
+    "description": "The mutual tolerance of the 1970s and 1980s is at an end",
+    "url": "http://www.economist.com/blogs/erasmus/2017/12/church-and-mafia",
+    "urlToImage": "https://cdn.static-economist.com/sites/default/files/20171216_BLP522.jpg",
+    "publishedAt": "2017-12-15T16:01:39Z"
+  }, {
+    "author": "The Economist",
+    "title": "The surreal and hyper-humane humour of “Lady Dynamite”",
+    "description": "The oft-overlooked show is a powerful take on mental health",
+    "url": "http://www.economist.com/blogs/prospero/2017/12/medicine-and-poison",
+    "urlToImage": "https://cdn.static-economist.com/sites/default/files/20171223_BKP501.jpg",
+    "publishedAt": "2017-12-18T14:39:00Z"
+  }, {
+    "author": "The Economist",
+    "title": "Obituary: Johnny Hallyday died on December 6th",
+    "description": "France’s answer to rock ’n’ roll was 74",
+    "url": "http://www.economist.com/news/obituary/21732510-frances-answer-rock-n-roll-was-74-obituary-johnny-hallyday-died-december-6th",
+    "urlToImage": "https://cdn.static-economist.com/sites/default/files/images/print-edition/20171216_OBP001_0.jpg",
+    "publishedAt": "2017-12-14T15:49:50Z"
+  }, {
+    "author": "The Economist",
+    "title": "Disney’s purchase of Fox’s entertainment assets is a gamble on media’s future",
+    "description": "In ceding ground, Rupert Murdoch is “pivoting at a pivotal moment”",
+    "url": "http://www.economist.com/news/business-and-finance/21732649-ceding-ground-rupert-murdoch-pivoting-pivotal-moment-disneys-purchase-foxs",
+    "urlToImage": "https://cdn.static-economist.com/sites/default/files/images/2017/12/articles/main/20171216_blp516.jpg",
+    "publishedAt": "2017-12-14T00:00:00Z"
+  }]
+}
