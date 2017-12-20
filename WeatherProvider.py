5145fa19ffa25df85f24306b6f10aa24aa03f12c
haoxiangpeng <3150102255@zju.edu.cn>
2017-12-19 22:58:40 -0800

    :sparkles: add weather info

---
+++ WeatherProvider.py
@@ -0,0 +1,55 @@
+import requests, json, sys
+from config import WEATHER_URL, CITY_CONFIG, tokens
+
+
+class WeatherProvider():
+  def __init__(self, city_config=CITY_CONFIG):
+    self._city_config = city_config
+    self._weather_info = {}
+    self._sensor_info = {}
+  
+  def _get_weather_from_web(self) -> dict:
+    for city, id in self._city_config.items():
+      weather_url = WEATHER_URL + '&id={id}&appid={appid}'.format(id=id, appid=tokens['open_weather'])
+      rv = requests.get(weather_url).content.decode()
+      rv = json.loads(rv)
+      self._weather_info[city] = rv
+    return self._weather_info
+  
+  def _get_weather_from_sensor(self) -> dict:
+    try:
+      import Adafruit_DHT
+      hum, temp = Adafruit_DHT.read_retry(11, 4)
+    except ImportError:
+      hum, temp = -1, -1
+    self._sensor_info['temp'] = temp
+    self._sensor_info['hum'] = hum
+    return self._sensor_info
+  
+  def _format_all(self) -> str:
+    rv = ''
+    for city, weather in self._weather_info.items():
+      rv += self._format_one_web_weather(weather)
+      rv += '\n----------------\n'
+    rv += self._format_sensor_weather()
+    return rv
+  
+  def _format_one_web_weather(self, weather_item: dict) -> str:
+    return '*{city}*\n' \
+           'Max temp: {max_temp}°C\n' \
+           'Min temp: {min_temp}°C\n' \
+           'Humidity: {hum}%\n' \
+           'Condition: {condition}'.format(
+      city=weather_item['name'], max_temp=weather_item['main']['temp_max'], min_temp=weather_item['main']['temp_min'],
+      hum=weather_item['main']['humidity'], condition=weather_item['weather'][0]['description']
+    )
+  
+  def _format_sensor_weather(self) -> str:
+    return '*Patrick\'s Home*\nTemp: {temp}°C\nHumidity: {humidity}%'.format(
+      temp=self._sensor_info['temp'], humidity=self._sensor_info['hum']
+    )
+  
+  def get_all_weather(self):
+    self._get_weather_from_sensor()
+    self._get_weather_from_web()
+    return self._format_all()
--- main.py
+++ main.py
@@ -1,40 +1,43 @@
 from Bot import Bot
 from NewsProvider import NewsProvider
+from WeatherProvider import WeatherProvider
 import os, sys
 
 CHANNEL_NAME = '@hlhstestchanel' if ('BOT_DEBUG' in os.environ) else '@newsathlh'
 
 
-def clean_message():
-  with Bot(CHANNEL_NAME) as bot:
-    bot.clean_channel()
+def clean_message(bot):
+  bot.clean_channel()
 
 
-def send_news():
+def send_news(bot):
   news = NewsProvider()
   rv = news.get_send_message()
-  with Bot(CHANNEL_NAME) as bot:
-    important = rv['important']
-    unimportant = rv['unimportant']
-    for item in important:
-      if not item: continue
-      bot.send_message(item)
-    for item in unimportant:
-      if not item: continue
-      bot.send_message(item, disable_preview=True, disable_notification=True, disable_web_page_preview=True)
+  
+  important = rv['important']
+  unimportant = rv['unimportant']
+  for item in important:
+    if not item: continue
+    bot.send_message(item)
+  for item in unimportant:
+    if not item: continue
+    bot.send_message(item, disable_web_page_preview=True)
 
 
-def send_weather():
-  pass
+def send_weather(bot):
+  weather = WeatherProvider()
+  rv = weather.get_all_weather()
+  bot.send_message(rv)
 
 
 if __name__ == '__main__':
-  if sys.argv[1] == 'all':
-    send_news()
-    send_weather()
-  elif sys.argv[1] == 'news':
-    send_news()
-  elif sys.argv[1] == 'weather':
-    send_weather()
-  
-  clean_message()
+  with Bot(CHANNEL_NAME) as bot:
+    if sys.argv[1] == 'all':
+      send_news(bot)
+      send_weather(bot)
+    elif sys.argv[1] == 'news':
+      send_news(bot)
+    elif sys.argv[1] == 'weather':
+      send_weather(bot)
+    
+    clean_message(bot)
